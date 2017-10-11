07d81a5abbf1e63bf852651d739f2d35443d2786
ilya0 <iosovets@gmail.com>
2017-10-10 18:26:42 -0700

    added working component

--- angular2-seed/app/courses.component.ts
+++ angular2-seed/app/courses.component.ts
@@ -2,7 +2,16 @@ import {Component} from 'angular2/core';
 
 @Component({
     selector:'courses', //specifies css selector for the host element
-    template: '<h2> Courses </h2>' //specifes the html that will be inserted into the dom when the html is rendered
+    template: `<h2> Courses </h2>
+    {{ title }}
+    <ul>
+        <li *ngFor="#course of courses">
+        {{ course }}
+        </li>
+    </ul>
+    ` //specifes the html that will be inserted into the dom when the html is rendered
 })
 export class CoursesComponent{
+    title: string = "The title of course page";
+    courses = ["Course1", "Course2", "Course3"];
 }
